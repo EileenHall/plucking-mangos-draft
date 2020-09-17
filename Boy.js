61ed5201dcc78df835d831d655cebc9393bb311f
eileen hall <68955914+EileenHall@users.noreply.github.com>
2020-09-17 14:28:49 +0530

    Add files via upload

---
+++ Boy.js
@@ -0,0 +1,9 @@
+class Boy extends BaseClass{
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("Plucking mangoes/boy.png");
+    }
+    display(){
+      super.display();
+    }
+  }
